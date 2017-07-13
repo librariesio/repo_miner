50f9e8a723eaf10af5e1719710e295e6af0f2111
Andrew Nesbitt <andrewnez@gmail.com>
2017-07-13 16:50:55 +0100

    Added dependency mining test

--- spec/repo_miner_spec.rb
+++ spec/repo_miner_spec.rb
@@ -6,19 +6,34 @@ RSpec.describe RepoMiner do
   end
 
   context "when mining the Libraries.io repository" do
-    let(:analyser) { RepoMiner.analyse('../libraries') }
-
-    it "does something useful" do
-      expect(analyser.length).to eq(4792)
-    end
+    let(:analyser) { RepoMiner.analyse('.') }
 
     it "mines email addresses" do
       initial_commit = analyser.first
-      expect(initial_commit.data).to eq({
-        :email => {
-          committer: 'andrewnez@gmail.com',
-          author: 'andrewnez@gmail.com'
-        }
+      expect(initial_commit.data[:email]).to eq({
+        committer: 'andrewnez@gmail.com',
+        author: 'andrewnez@gmail.com'
+      })
+    end
+
+    it "mines dependencies" do
+      initial_commit = analyser.first
+      expect(initial_commit.data[:dependencies]).to eq({
+        :added_manifests => [
+          {
+            :path => "repo_miner.gemspec",
+            :platform => "rubygems",
+            :added_dependencies => [
+              {:name=>"bundler", :requirement=>"~> 1.14", :type=>:development},
+              {:name=>"rake", :requirement=>"~> 10.0", :type=>:development},
+              {:name=>"rspec", :requirement=>"~> 3.0", :type=>:development}
+            ],
+            :modified_dependencies=>[],
+            :removed_dependencies=>[]
+          }
+        ],
+        :modified_manifests => [],
+        :removed_manifests => [],
       })
     end
   end
